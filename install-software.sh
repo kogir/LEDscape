80e6227c6cfa1e0c6db55ee06f63700f7e91f675
Nick Sivo <nick@kogir.com>
2018-07-10 01:19:25 +0000

    Update for latest bbb-9.4 image.

--- install-software.sh
+++ install-software.sh
@@ -1,47 +1,24 @@
 #!/usr/bin/env bash
 
-if [[ ! -f ledscape.c ]]; then
+function finish {
+	popd
+}
+trap finish EXIT
 
-	echo "Could not find ledscape.c. Must run from inside the LEDscape directory."
-	exit -1
-fi
+pushd $(dirname $0)
 
 echo "Making ledscape..."
 make
 
-if [[ ! -f "/boot/dtbs/$(uname -r)/am335x-boneblack.dtb" ]]; then
+if [[ -f "/etc/ledscape-config.json" ]]; then
 
-	echo "Could not find "/boot/dtbs/$(uname -r)/am335x-boneblack.dtb". Only works with the Wheezy version of Debian."
-	exit -1
+	echo "Leaving existing /etc/ledscape-config.json intact"
 
-fi
-
-
-
-echo "Making backups of old device tree files..."
-mkdir /boot/dtbs/$(uname -r)/ledscape.bak
-cp /boot/dtbs/$(uname -r)/am335x-boneblack.dtb{,.preledscape_bk}    
-cp /boot/dtbs/$(uname -r)/am335x-bonegreen.dtb{,.preledscape_bk}    
-
-echo "Copying new device tree files..."		
-cp devicetree/am335x-boneblack.dtb /boot/dtbs/$(uname -r)/ 
-cp devicetree/am335x-bonegreen.dtb /boot/dtbs/$(uname -r)/ 
-
-echo Copying config file to /etc
-
-if [[ -f "//etc/ledscape-config.json" ]]; then
-
-	echo Leaving existing /etc/ledscape-config.json intact. 
-	
 else
 
-	cp configs/ws281x-config.json /etc/ledscape-config.json
-	
+	echo "Copying config file to /etc"
+	sudo cp configs/ws281x-config.json /etc/ledscape-config.json
+
 fi
 
-echo "Enabling kernel module..."	
-modprobe uio_pruss  
-
-
-echo "Done. Please enter reboot to reboot the machine and enable changes."
-		
+echo "Done"
